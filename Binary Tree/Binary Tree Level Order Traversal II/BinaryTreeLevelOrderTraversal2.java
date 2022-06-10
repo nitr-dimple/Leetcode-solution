673acd36a535532ba1b3d4e866ac6a1d62ecd13f
Dimple Patel <dimplepatel982@gmail.com>
2022-06-09 21:05:54 -0400

    [Binary Tree][Binary Tree Level Order Traversal2]

---
+++ Binary Tree/Binary Tree Level Order Traversal II/BinaryTreeLevelOrderTraversal2.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> list = new ArrayList<>();
+        queue.add(root);
+        while(queue.size() != 0){
+            int len = queue.size();
+            List<Integer> l = new ArrayList<>();
+
+            for(int i = 0; i < len; i++){
+                TreeNode node = queue.remove();
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+                l.add(node.val);
+            }
+            list.add(0,l);
+        }
+        return list;
+    }
+}
