ceb61d892a9f52b073d758dde1feb9646f4e1ca6
Dimple Patel <dimplepatel982@gmail.com>
2022-09-07 18:33:39 -0400

    [Queue][Number of Recent Calls]

---
+++ Queue/Number of Recent Call/NumberOfRecentCall.java
@@ -0,0 +1,20 @@
+class RecentCounter {
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<Integer>();
+    }
+    
+    public int ping(int t) {
+        queue.add(t);
+        while(!queue.isEmpty() && queue.peek() < t-3000)
+            queue.remove();
+        return queue.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
