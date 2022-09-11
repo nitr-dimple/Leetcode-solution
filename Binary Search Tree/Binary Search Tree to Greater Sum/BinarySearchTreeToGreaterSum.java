7e2c752e90600c7a068e29599e7dbcac2d79281c
Dimple Patel <dimplepatel982@gmail.com>
2022-09-11 00:27:43 -0400

    [Binary Search Tree][Binary Search Tree to Greater Sum]

---
+++ Binary Search Tree/Binary Search Tree to Greater Sum/BinarySearchTreeToGreaterSum.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int currentSum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        calculate(root);
+        return root; 
+    }
+    public void calculate(TreeNode root){
+        if(root != null){
+            calculate(root.right);
+            root.val += currentSum;
+            currentSum = root.val;
+            calculate(root.left);
+        }
+    }
+}
