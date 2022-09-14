8778c1b0a43572db64d9d6bef881de935da5cfb3
Dimple Patel <dimplepatel982@gmail.com>
2022-09-14 16:41:15 -0400

    [Graph][Find Center of Star Graph]

---
+++ Graph/Find Center of Star Graph/FindCenterOfStarGraph.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int findCenter(int[][] edges) {
+        if(edges[1][0] == edges[0][0] || edges[1][1] == edges[0][0]) return edges[0][0];
+        else return edges[0][1];
+    }
+}
