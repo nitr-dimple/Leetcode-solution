230c18b21dd5b7db2a91e2be7e8cb35439233883
Dimple Patel <dimplepatel982@gmail.com>
2023-01-06 20:45:00 -0500

    [Linked List][Sort List]

---
+++ Linked List/Sort List/README.md
@@ -0,0 +1,31 @@
+# Problem
+
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+### Example 1:
+
+```
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+```
+
+### Example 2:
+```
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+```
+
+### Example 3:
+```
+Input: head = []
+Output: []
+```
+
+### Constraints:
+
+- The number of nodes in the list is in the range [0, 5 * 10 <sup> 4 </sup>].
+- -10 <sup>5</sup> <= Node.val <= 10<sup>5</sup>
+
+#### Approach
+
+- Used merge sort algorithm
---
+++ Linked List/Sort List/SortList.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode temp = head;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            temp = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        temp.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+
+        return merge(left, right);
+    }
+
+    public ListNode merge(ListNode left, ListNode right){
+        ListNode sortedList = new ListNode(0);
+        ListNode temp = sortedList;
+
+        while(left != null || right!= null){
+            if(left == null) {
+                temp.next = right;
+                break;
+            }
+            if(right == null) {
+                temp.next = left;
+                break;
+            }
+            if(left.val <= right.val){
+                temp.next = left;
+                left = left.next;
+            }else{
+                temp.next = right;
+                right = right.next;
+            }
+            temp = temp.next;
+        }
+        return sortedList.next;
+    }
+}
