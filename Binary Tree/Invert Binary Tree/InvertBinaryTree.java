2c96307f47194d197cb2edeb9c658af0ef6cc23e
Dimple Patel <dimplepatel982@gmail.com>
2023-06-01 20:02:28 -0400

    [Binary Tree][Invert Binary Tree ]

--- Binary Tree/Invert Binary Tree/InvertBinaryTree.java
+++ Binary Tree/Invert Binary Tree/InvertBinaryTree.java
@@ -15,18 +15,25 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null) return root;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while(queue.size()!= 0){
-            TreeNode t = queue.remove();
-            TreeNode temp = t.left;
-            t.left = t.right;
-            t.right = temp;
-            if(t.left != null)
-                queue.add(t.left);
-            if(t.right != null)
-                queue.add(t.right);
+        // if(root == null) return root;
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // queue.add(root);
+        // while(queue.size()!= 0){
+        //     TreeNode t = queue.remove();
+        //     TreeNode temp = t.left;
+        //     t.left = t.right;
+        //     t.right = temp;
+        //     if(t.left != null)
+        //         queue.add(t.left);
+        //     if(t.right != null)
+        //         queue.add(t.right);
+        // }
+        if(root != null){
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            invertTree(root.left);
+            invertTree(root.right);
         }
         return root;
         
