ae0723ad7f2b3f8ea2727fac77eb970300c505c2
Dimple Patel <dimplepatel982@gmail.com>
2023-06-11 22:30:46 -0400

    [Binary Tree][Find Bottom Left Tree Value]

---
+++ Binary Tree/Find Bottom Left Tree Value/Find Bottom Left Tree Value.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        queue.add(root);
+        int leftValue = root.val;
+        int length = queue.size();
+        while(queue.size() != 0){
+            leftValue = queue.peek().val;
+            for(int i=0; i< length; i++){
+                TreeNode temp = queue.poll();
+                if(temp.left != null) queue.add(temp.left);
+                if(temp.right != null) queue.add(temp.right);
+            }
+            length = queue.size();
+        }
+        return leftValue;
+        
+    }
+}
---
+++ Binary Tree/Find Bottom Left Tree Value/README.md
@@ -0,0 +1,22 @@
+# Problem
+
+Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+### Example 1:
+
+```
+Input: root = [2,1,3]
+Output: 1
+```
+
+### Example 2:
+```
+Input: root = [1,2,3,4,null,5,6,null,null,7]
+Output: 7
+```
+
+### Constraints:
+
+- TThe number of nodes in the tree is in the range [1, 10<sup>4</sup>].
+- -2<sup>31</sup> <= Node.val <= 2<sup>31</sup> - 1
+
