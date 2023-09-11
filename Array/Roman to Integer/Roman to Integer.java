c5a73e0d85ff310a90711eba34c72ed9eb56619b
Dimple Patel <dimplepatel982@gmail.com>
2023-09-11 15:13:14 -0400

    [Array][Roman to Integer]

---
+++ Array/Roman to Integer/Roman to Integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int romanToInt(String s) {
+
+        int ans = 0;
+        for(int i=0; i<s.length()-1; i++){
+            if(getNumericalVal(s.charAt(i)) >=getNumericalVal(s.charAt(i+1))) ans+=getNumericalVal(s.charAt(i));
+            else ans -= getNumericalVal(s.charAt(i));
+        }
+        ans+= getNumericalVal(s.charAt(s.length()-1));
+        return ans;
+    }
+
+    public int getNumericalVal(Character c){
+        switch (c){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+}
